a3de3e6d78aa0762683db1b3119aef7c98810216
Braden Church <bradenchurch@gmail.com>
2020-06-24 18:38:17 -0600

    added routes

---
+++ src/components/shared/NoMatch.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NoMatch = () => (
+  <>
+  <h1>404 Page Not Found</h1>
+  <Link to='/'>
+    Return Home
+  </Link>
+  </>
+
+)
+
+export default NoMatch;
